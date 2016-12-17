00e0673157036d1b1861a771ddde55f3dd811c2f
qing@ciandt.com <qing@ciandt.com>
2016-12-17 19:52:15 +0800

    index

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,25 +12,57 @@ Page({
     shops:[],
     random_shop_id:'',
     random_shop_name:'',
-    myAddress: {
-      array: ['美国', '中国', '巴西', '日本'],
+    myAddressPicker: {
+      array: ['家', '单位'],
       index: 0
-    }
+    },
+    myAddress:[],
   },
+
   bindPickerChange: function(e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
+    var page = this;
     this.setData({
-      index: e.detail.value
+      address: page.data.myAddress[e.detail.value][2]
     })
   },
-  durationChange: function(e) {
-    this.setData({
-      duration: e.detail.value
-    })
-  },
+
   onLoad:function(options){
-    var page = this
-        wx.getLocation({
+    var page = this;
+    var myAddressdata = "0###家###印象城|||1###公司###和邦大厦";
+    console.log(myAddressdata);
+    var data_result = new Array();
+    var data_array = myAddressdata.split("|||");
+    for (var i = data_array.length-1; i >=0 ; i--) {
+      var store_detail_array = data_array[i].split("###");
+      data_result[data_array.length-i-1] = store_detail_array;
+    };
+    page.data.myAddress = data_result;
+    console.log(data_result);
+
+
+    // wx.getStorage({
+    //   key: 'myaddress',
+    //   success: function(res) {
+    //     res.data = "0###家###鄞州区印象城|||1###公司###和邦大厦"
+    //     if (res.data) {
+    //       var data_result = new Array();
+    //       var data_array = res.data.split("|||");
+    //       for (var i = data_array.length-1; i >=0 ; i--) {
+    //         var store_detail_array = data_array[i].split("###");
+    //         data_result[data_array.length-i-1] = store_detail_array;
+    //       };
+    //       page.data.myAddress = data_result;
+    //       console.log(data_result);
+    //     }else{
+    //       console.log('None');
+    //     };
+    //   },
+    //   fail: function() {
+    //     console.log('Fail');
+    //   }
+    // })
+    wx.getLocation({
+
       type: 'wgs84',
       success: function(res) {
         page.setData({
@@ -67,29 +99,30 @@ Page({
       }
     })
   },
+
   randomShop: function(e) {
-   var page = this
-   var keyword = page.data.address
-   wx.request({
-       url: 'http://www.dianping.com/search/map/ajax/json',
-       data: 'cityId=11&cityEnName=ningbo&shopType=10&categoryId=10&shopSortItem=1&keyword='+keyword+'&searchType=1',
-       method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-       header: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-       }, // 设置请求的 header
-       success: function(res){
-          // success
-          // console.log('success')
-          page.setData({
-              shops: res.data.shopRecordBeanList
-          })
-        },
-        fail: function() {
-                // fail
-        },
-        complete: function() {
-                // complete
-        }
+    var page = this
+    var keyword = page.data.address
+    wx.request({
+      url: 'http://www.dianping.com/search/map/ajax/json',
+      data: 'cityId=11&cityEnName=ningbo&shopType=10&categoryId=10&shopSortItem=1&keyword='+keyword+'&searchType=1',
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }, // 设置请求的 header
+      success: function(res){
+        // success
+        // console.log('success')
+        page.setData({
+            shops: res.data.shopRecordBeanList
+        })
+      },
+      fail: function() {
+              // fail
+      },
+      complete: function() {
+              // complete
+      }
     })
    //生成0到19的随机数
    var randomNum = Math.ceil(Math.random()*19)
@@ -99,7 +132,6 @@ Page({
      random_shop_id:page.data.shops[randomNum].shopId,
      random_shop_name:page.data.shops[randomNum].shopName
    })
-
     var targetUrl = '/pages/detail/detail'
     if(e.currentTarget.dataset.shopId != null)
       targetUrl = targetUrl + '?shopId=' + e.currentTarget.dataset.shopId+'&shopName='+e.currentTarget.dataset.shopName
