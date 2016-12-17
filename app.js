cdbdec2167d26165961611ecabb6d8168b3ae55b
cato <yechaozheng@ciandt.com>
2016-12-17 10:28:50 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+App({
+    globalData: 2,
+    onLaunch: function() {
+        console.log(111);
+    },
+    onShow: function() {
+        console.log(222);
+    },
+    onHide: function() {
+        console.log(333);
+    }
+})
