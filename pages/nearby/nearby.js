088a052754724f4306ba4e6d85e7cfdde52bbb8a
Hardy <hardy@HardydeMacBook-Pro.local>
2016-12-17 15:42:11 +0800

    nearby style

--- pages/nearby/nearby.js
+++ pages/nearby/nearby.js
@@ -6,6 +6,15 @@ Page({
     shops:[]
   },
 
+  onItemClick(e){
+    var targetUrl = '/pages/detail/detail'
+    if(e.currentTarget.dataset.shopId != null)
+      targetUrl = targetUrl + '?shopId=' + e.currentTarget.dataset.shopId+'&shopName='+e.currentTarget.dataset.shopName
+    wx.navigateTo({
+      url: targetUrl
+    })
+  },
+
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
     var page = this
