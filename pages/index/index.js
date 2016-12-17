7b1c2f5d9e2c4d44a728068e619d6b29fb96b7f1
Hardy <hardy@HardydeMacBook-Pro.local>
2016-12-17 14:11:09 +0800

    index style

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,35 +1,27 @@
 'use strict'
 
+
+
 Page({
   data: {
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
-    indicatorDots: false,
-    autoplay: false,
-    interval: 5000,
-    duration: 1000
-  },
-  changeIndicatorDots: function(e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
-  changeAutoplay: function(e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
-  intervalChange: function(e) {
-    this.setData({
-      interval: e.detail.value
-    })
+    locationSrc: '../../image/icon_index_white.png',
+    addressSrc: '../../image/icon_address_white.png'
   },
   durationChange: function(e) {
     this.setData({
       duration: e.detail.value
+    })
+  },
+  getLocation: function(e) {
+    wx.getLocation({
+      type: 'wgs84',
+      success: function(res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        var speed = res.speed
+        var accuracy = res.accuracy
+        console.log(res);
+      }
     })
   }
 })
