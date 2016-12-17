9aedda83f6d7b0e3b548a1eedcd33ca48792bc43
qing@ciandt.com <qing@ciandt.com>
2016-12-17 16:59:40 +0800

    add

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,6 +1,8 @@
 // pages/my/my.js
 Page({
-  data:{},
+  data:{
+    myfavorite_shops:[],
+  },
   onLoad:function(options){
     
   },
@@ -9,19 +11,18 @@ Page({
   },
   onShow:function(){
     var page = this;
+    
     wx.getStorage({
       key: 'myfavorite',
       success: function(res) {
         if (res.data) {
           var data_result = new Array();
           var data_array = res.data.split("|||");
-          for (var i = 0; i < data_array.length; i++) {
+          for (var i = data_array.length-1; i >=0 ; i--) {
             var store_detail_array = data_array[i].split("###");
-            data_result['num_'+store_detail_array[4]+"_"+store_detail_array[0]] = store_detail_array;
+            data_result[data_array.length-i-1] = store_detail_array;
           };
-          page.setData({
-            myfavorite_shops: data_result
-          })
+          page.data.myfavorite_shops = data_result;
           console.log(data_result);
         }else{
           console.log('None');
@@ -39,9 +40,9 @@ Page({
   },
   clearFavorite:function(){
     try {
-        wx.removeStorageSync('myfavorite')
+      wx.removeStorageSync('myfavorite')
     } catch(e) {
-      // Do something when catch error
+      console.log('fail');
     }
   }
 })
